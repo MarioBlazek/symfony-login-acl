3eb3b4a0fb1311b1f8e566817b9d3e753e8ee6c0
Marek <blazek.mario@gmail.com>
2015-05-03 23:56:07 +0200

    Role command

---
+++ src/App/Bundle/LoginAndACLBundle/Command/RoleCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Bundle\LoginAndACLBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RoleCommand extends ContainerAwareCommand
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function configure()
+	{
+		$this->setName('acl:role:create');
+		$this->setDescription('Create new role.');
+		$this->setDefinition(array(
+			new InputArgument('roleName', InputArgument::REQUIRED, 'Role name.'),
+			new InputOption('group', 'g', InputOption::VALUE_REQUIRED, 'Group name.'),
+		));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$roleName = $input->getArgument('roleName');
+		$group = $input->getOption('group');
+
+		$roleHandler = $this->getContainer()->get('acl.role.handler');
+
+		$roleHandler->create($roleName, $group);
+
+		$output->writeln(sprintf('Role <comment>%s</comment> created.', $roleName));
+	}
+}
