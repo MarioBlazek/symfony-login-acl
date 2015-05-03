07c50889bc29e5160a14560d983af5afded0e736
Marek <blazek.mario@gmail.com>
2015-05-03 23:53:34 +0200

    Login implemented

--- src/App/Bundle/LoginAndACLBundle/Controller/LoginController.php
+++ src/App/Bundle/LoginAndACLBundle/Controller/LoginController.php
@@ -3,9 +3,16 @@
 namespace App\Bundle\LoginAndACLBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class LoginController extends Controller
 {
+	/**
+	 * Show login form
+	 *
+	 * @return Response
+	 */
 	public function showLoginFormAction()
 	{
 		$authenticationUtils = $this->get('security.authentication_utils');
@@ -18,8 +25,26 @@ class LoginController extends Controller
 		));
 	}
 
+	/**
+	 * Login check action
+	 */
 	public function loginCheckAction()
 	{
 
+	}
+
+	/**
+	 * Display homepage after successful login
+	 */
+	public function showHomepageAfterLoginAction()
+	{
+		$securityAuthCheck = $this->get('security.authorization_checker');
+		if (!$securityAuthCheck->isGranted('IS_AUTHENTICATED_FULLY')) {
+			throw $this->createAccessDeniedException();
+		}
+
+		return $this->render('ACLBundle:Homepage:index.html.twig', array(
+			'user' => $this->getUser(),
+		));
 	}
 }
